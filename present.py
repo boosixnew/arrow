3b2b391ca5489a17820ca373cdb397c0ce665f6f
Ivan <149861268+boosixnew@users.noreply.github.com>
2023-11-27 10:08:34 +0100

    new

---
+++ present.py
@@ -0,0 +1,12 @@
+Copyright (c) 2017 to present Pydantic Services Inc. and individual contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+in the Software without restriction, including without limitation the rights
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+SOFTWARE.
